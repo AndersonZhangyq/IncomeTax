ee5a480c6ccf421482205339462c3cca781765bb
AndersonZhangyq <andersonzhangyq@gmail.com>
2021-10-01 09:10:07 +0800

    update deploy.js

--- gh-pages-deploy.js
+++ gh-pages-deploy.js
@@ -3,18 +3,19 @@ const execa = require("execa");
 const fs = require("fs");
 (async () => {
     try {
+        await execa("git", ["branch", "-D", "gh-pages"]);
         await execa("git", ["checkout", "--orphan", "gh-pages"]);
         // eslint-disable-next-line no-console
         console.log("Building started...");
-        await execa("npm", ["run", "build"]);
+        await execa("quasar", ["build"]);
         // Understand if it's dist or build folder
-        const folderName = fs.existsSync("dist") ? "dist" : "build";
+        const folderName = fs.existsSync("dist") ? "dist\\spa" : "build\\spa";
         await execa("git", ["--work-tree", folderName, "add", "--all"]);
         await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
         console.log("Pushing to gh-pages...");
         await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
         await execa("rm", ["-r", folderName]);
-        await execa("git", ["checkout", "-f", "master"]);
+        await execa("git", ["checkout", "-f", "main"]);
         await execa("git", ["branch", "-D", "gh-pages"]);
         console.log("Successfully deployed, check your settings");
     } catch (e) {
